2eb13bcc87dc9227999a7b7fa65cc2b46b72520e
CelestinPL <136016689+CelestinPL@users.noreply.github.com>
2023-09-30 00:21:53 +0200

    Add files via upload

---
+++ Title/e_title.js
@@ -0,0 +1,143 @@
+var getScriptPromisify = (src) => {
+  // Workaround with conflict between geo widget and echarts.
+  
+  return new Promise(resolve => {
+    $.getScript(src, () => {
+      resolve()
+    })
+  })
+}
+
+(function() {
+  const template = document.createElement('template');
+  template.innerHTML = `
+    <style>
+      :host > #chart {
+        width: 100%;
+        height: 100%;
+      }
+    </style>
+    <div id="root" style="width: 100%; height: 100%;">
+      <div id="chart"></div>
+    </div>
+  `;
+    
+  class Renderer {
+    constructor (root) {
+      this._root = root
+      this._echart = null
+    }
+    
+    render (dataBinding) {
+      this.dispose()
+
+      if (dataBinding.state !== 'success') { return }
+
+      let { data, metadata } = dataBinding
+
+      this._echart = echarts.init(this._root)
+      this._echart.setOption({
+        graphic: {
+          elements: [
+            {
+              type: 'text',
+              left: 'center',
+              top: '10px',
+              style: {
+                text: 'Rekomendator rumów',
+                fontSize: 80,
+                fontFamily: 'cursive',
+                fontWeight: 'bold',
+                lineDash: [0, 200],
+                lineDashOffset: 0,
+                fill: 'transparent',
+                stroke: '#FFC933',
+                lineWidth: 1
+              },
+              keyframeAnimation: {
+                duration: 3000,
+                loop: true,
+                keyframes: [
+                  {
+                    percent: 0.7,
+                    style: {
+                      fill: 'transparent',
+                      lineDashOffset: 200,
+                      lineDash: [200, 0]
+                    }
+                  },
+                  {
+                    // Stop for a while.
+                    percent: 0.8,
+                    style: {
+                      fill: 'transparent'
+                    }
+                  },
+                  {
+                    percent: 0.9,
+                    style: {
+                      fill: '#FFC933'
+                    }
+                  }
+                ]
+              }
+            }
+          ]
+        }
+      })
+    }
+
+    dispose () {
+      if (this._echart) {
+        echarts.dispose(this._echart)
+      }
+    }
+  }
+  
+  class GraphMain extends HTMLElement {
+    constructor () {
+      super();
+
+      this._shadowRoot = this.attachShadow({ mode: 'open' });
+      this._shadowRoot.appendChild(template.content.cloneNode(true));
+      this._root = this._shadowRoot.getElementById('root');
+      this._renderer = new Renderer(this._root);
+    }
+
+    // ------------------
+    // LifecycleCallbacks
+    // ------------------
+    async onCustomWidgetBeforeUpdate (changedProps) {
+    }
+
+    async onCustomWidgetAfterUpdate (changedProps) {
+      this.render();
+    }
+
+    async onCustomWidgetResize (width, height) {
+      this.render();
+    }
+
+    async onCustomWidgetDestroy () {
+      this.dispose();
+    }
+
+    async render () {
+      await getScriptPromisify('https://fastly.jsdelivr.net/npm/echarts@5/dist/echarts.min.js')
+      
+      if (!document.contains(this)) {
+        // Delay the render to assure the custom widget is appended on dom
+        setTimeout(this.render.bind(this), 0);
+        return;
+      }
+      
+      this._renderer.render(this.dataBinding);
+    }
+
+    dispose () {
+      this._renderer.dispose();
+    }
+  }
+
+  customElements.define('com-senfiniti-sac-echarts-title', TitleMain);
+})();
