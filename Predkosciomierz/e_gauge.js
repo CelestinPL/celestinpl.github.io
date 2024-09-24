3651880a50907594341a80322e71f87406845f58
CelestinPL <136016689+CelestinPL@users.noreply.github.com>
2024-09-24 02:06:02 +0200

    Create e_gauge.js

---
+++ Predkosciomierz/e_gauge.js
@@ -0,0 +1,166 @@
+var getScriptPromisify = (src) => {
+  // Workaround with conflict between geo widget and echarts.
+  
+  return new Promise(resolve => {
+    $.getScript(src, () => {
+      resolve()
+    })
+  })
+}
+
+(function() {
+  const template = document.createElement('template');
+  template.innerHTML = `
+    <style>
+      :host > #chart {
+        width: 100%;
+        height: 100%;
+      }
+    </style>
+    <div id="root" style="width: 100%; height: 100%;">
+      <div id="chart"></div>
+    </div>
+  `;
+    
+  class Renderer {
+    constructor (root) {
+      this._root = root
+      this._echart = null
+    }
+    
+    parseMetadata (metadata) {
+      const { dimensions: dimensionsMap, mainStructureMembers: measuresMap } = metadata
+      const dimensions = []
+      for (const key in dimensionsMap) {
+        const dimension = dimensionsMap[key]
+        dimensions.push({ key, ...dimension })
+      }
+      const measures = []
+      for (const key in measuresMap) {
+        const measure = measuresMap[key]
+        measures.push({ key, ...measure })
+      }
+      return { dimensions, measures, dimensionsMap, measuresMap }
+    }
+    
+    render (dataBinding) {
+      this.dispose()
+
+      if (dataBinding.state !== 'success') { return }
+
+      let { data, metadata } = dataBinding
+      const { dimensions, measures } = this.parseMetadata(metadata)
+
+      const [measure] = measures
+
+      this._echart = echarts.init(this._root)
+      this._echart.setOption({
+          series: [
+            {
+              type: 'gauge',
+              min: 0,
+              max: 3,
+              axisLine: {
+                lineStyle: {
+                  width: 30,
+                  color: [
+                    [0.7, '#EC7063'],
+                    [0.9, '#5DADE2'],
+                    [1, '#58D68D']
+                  ]
+                }
+              },
+              pointer: {
+                itemStyle: {
+                  color: 'auto'
+                }
+              },
+              axisTick: {
+                distance: -30,
+                length: 8,
+                lineStyle: {
+                  color: '#fff',
+                  width: 2
+                }
+              },
+              splitLine: {
+                distance: -30,
+                length: 30,
+                lineStyle: {
+                  color: '#fff',
+                  width: 4
+                }
+              },
+              axisLabel: {
+                color: 'inherit',
+                distance: 40,
+                fontSize: 10
+              },
+              detail: {
+                valueAnimation: true,
+                color: 'inherit'
+              },
+              data: [
+                {
+                  value: +(data[0][measure.key]['raw']).toFixed(2)
+                }
+              ]
+            }
+          ]
+      })
+    }
+
+    dispose () {
+      if (this._echart) {
+        echarts.dispose(this._echart)
+      }
+    }
+  }
+  
+  class GaugeMain extends HTMLElement {
+    constructor () {
+      super();
+
+      this._shadowRoot = this.attachShadow({ mode: 'open' });
+      this._shadowRoot.appendChild(template.content.cloneNode(true));
+      this._root = this._shadowRoot.getElementById('root');
+      this._renderer = new Renderer(this._root);
+    }
+
+    // ------------------
+    // LifecycleCallbacks
+    // ------------------
+    async onCustomWidgetBeforeUpdate (changedProps) {
+    }
+
+    async onCustomWidgetAfterUpdate (changedProps) {
+      this.render();
+    }
+
+    async onCustomWidgetResize (width, height) {
+      this.render();
+    }
+
+    async onCustomWidgetDestroy () {
+      this.dispose();
+    }
+
+    async render () {
+      await getScriptPromisify('https://fastly.jsdelivr.net/npm/echarts@5/dist/echarts.min.js')
+      
+      if (!document.contains(this)) {
+        // Delay the render to assure the custom widget is appended on dom
+        setTimeout(this.render.bind(this), 0);
+        return;
+      }
+      
+      this._renderer.render(this.dataBinding);
+    }
+
+    dispose () {
+      this._renderer.dispose();
+    }
+  }
+
+  customElements.define('com-senfiniti-sac-echarts-predkosciomierz', GaugeMain);
+})();
